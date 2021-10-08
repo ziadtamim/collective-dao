d9212162d5233e8a8440ba0a2310ef439fbf7976
Mohammed <mohams3ios01@gmail.com>
2021-10-08 10:39:39 -0400

    Add NounsService framework

---
+++ app/NounsServices/NounsServices.h
@@ -0,0 +1,18 @@
+//
+//  NounsServices.h
+//  NounsServices
+//
+//  Created by Mohammed Ibrahim on 2021-10-08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NounsServices.
+FOUNDATION_EXPORT double NounsServicesVersionNumber;
+
+//! Project version string for NounsServices.
+FOUNDATION_EXPORT const unsigned char NounsServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NounsServices/PublicHeader.h>
+
+
--- app/NounsServices/README.md
+++
@@ -1,3 +0,0 @@
-# NounsServices
-
-A description of this package.
