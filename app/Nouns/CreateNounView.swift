50da84ceae05b6b63ddb9114e671b5cc29d83f5a
Mohammed <mohams3ios01@gmail.com>
2021-10-11 18:55:25 -0400

    Initial

---
+++ app/Nouns/CreateNounView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateNounView.swift
+//  Nouns
+//
+//  Created by Mohammed Ibrahim on 2021-10-11.
+//
+
+import SwiftUI
+
+struct CreateNounView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateNounView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateNounView()
+    }
+}
